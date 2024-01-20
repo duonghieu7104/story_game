f341a124a2f25b787b8930b11002809fadaf29b9
HieuDuong <duonghieu.7104@gmail.com>
2024-01-20 19:11:33 +0700

    gpt chat
    transfer gpt_create_story to gpt_chat, because gpt api does not save conversation. gpt_chat will be the chat box that supports the player when selecting visual novel

--- GPT_create_story/script/gpt_chat.gd
+++ GPT_create_story/script/gpt_chat.gd
@@ -8,17 +8,14 @@ var headers = ["Content-Type: application/json", "Authorization: Bearer " + api_
 var model : String = "gpt-3.5-turbo"
 var messages = []
 var request : HTTPRequest
-var scene_path = load("res://GPT_create_story/scene/chat_box.tscn")
 var current_mess : String
-var instances_array = []
-var save_path = "user://conversations.json" 
 
 func _ready():
 	request = HTTPRequest.new()
 	add_child(request)
 	request.connect("request_completed", _on_request_completed)
 	
-	dialogue_request("")
+	dialogue_request("request")
 
 func dialogue_request(player_dialogue):
 	messages.append({
@@ -42,26 +39,4 @@ func _on_request_completed(result, response_code, headers, body):
 	json.parse(body.get_string_from_utf8())
 	var response = json.get_data()
 	var message = response["choices"][0]["message"]["content"]
-	#current_mess =
 	print(message)
-	#save_conversations_to_file(message)
-	
-	
-	#var first_time : bool = true
-	#if message == "CÓ" and first_time:
-		#dialogue_request("BẮT ĐẦU")
-		#first_time = false
-	#else:
-		#pass
-
-func create_ui_for_gpt():
-	pass
-
-# Định nghĩa hàm để lưu các cuộc trò chuyện vào file
-func save_conversations_to_file(data):
-	var file = FileAccess.open(save_path, FileAccess.WRITE)
-	var json_string = JSON.stringify(data, "\t")
-	file.store_string(json_string)
-	file.close()
-	print("Conversations saved to file.")
-
