9d62d19e69ba31c849e7fa9192c74ad7fc46dba3
HieuDuong <duonghieu.7104@gmail.com>
2024-01-28 12:23:48 +0700

    load enemy

---
+++ Enemy/enemy.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name Enemy
+
+@export var name : String
+@export var rank : int
+@export var hp_max : int
+@export var spd : int
+@export var atk : int
+@export var crichance : int
+@export var cridamge : int
+@export var avatar_path : String
--- Turn_base/script/turnbase.gd
+++ Turn_base/script/turnbase.gd
@@ -2,11 +2,32 @@ extends Control
 
 signal text_box
 
+@export var name_enemy : String
+@export var rank : int
+@export var hp_max : int
+@export var spd : int
+@export var atk : int
+@export var crichance : int
+@export var cridamge : int
+@export var avatar_path : String
+
 func _ready():
 	showMasage(" ")
+	$VBoxContainer/Enemy.texture = load(avatar_path)
 
 func _process(delta):
 	pass
+
+func load_enemy(path):
+	var enemy = ResourceLoader.load(path)
+	name_enemy = enemy.name
+	rank = enemy.rank
+	hp_max = enemy.hp_max
+	spd = enemy.spd
+	atk = enemy.atk
+	crichance = enemy.crichance
+	cridamge = enemy.cridamge
+	avatar_path = enemy.avatar_path
 
 func showMasage(text):
 	$TextHere.text = text
